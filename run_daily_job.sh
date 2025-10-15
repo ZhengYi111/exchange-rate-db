22d2cb5f7f86c4ee748489cbc95148fd13d77bd5
Vive <15870580708@163.com>
2025-10-15 16:57:45 +0800

    更新定时任务脚本

--- run_daily_job.sh
+++ run_daily_job.sh
@@ -2,7 +2,7 @@
 cd /Users/zhengyi/workspace/exchange_rate_project
 
 # 激活虚拟环境
-source .venv/bin/activate
+source venv/bin/activate
 
 # 执行 Python 脚本 & 追加日志
 python src/fetch_and_store.py >> logs/run.log 2>&1
